d1437a6c382f6db504a8c32b9428000a8625cc26
Thearcane <cvxanonymous007@gmail.com>
2023-01-24 10:11:52 +0000

    feat: login account.

--- src/ProtonMail.ts
+++ src/ProtonMail.ts
@@ -1,4 +1,4 @@
-import puppeteer from "puppeteer";
+import puppeteer, { Page, Browser } from "puppeteer";
 
 interface UserConfig {
     username: string,
@@ -9,10 +9,12 @@ class ProtonMail {
 
     _Username : string;
     _Password : string;
-    _BROWSER : any;
-    _PAGE : any;
+    _BROWSER : Promise<Browser> | any;
+    _PAGE : Promise<Page> | any;
 
     constructor(config : UserConfig) {
+        this._BROWSER = null;
+        this._PAGE = null;
         if(!config.username) throw new Error('Username not found');
         if(!config.password) throw new Error('Password not found')
         this._Username = config.username || '';
@@ -21,12 +23,29 @@ class ProtonMail {
     }
 
     async congifurePuppeteer() {
-        this._BROWSER = await puppeteer.launch({ headless: true })
-        this._PAGE = await this._BROWSER.newPage();
-        await this._PAGE.goto('https://www.typescriptlang.org/docs/handbook/esm-node.html/')
-        await this._PAGE.screenshot({path: '1.png'})
+        const _BROWSER = await puppeteer.launch({ headless: true })
+        const _PAGE = await _BROWSER.newPage();
+        await this.loginProton(_PAGE)
     }
 
+    async loginProton(PAGE : Page) {
+        let invalidLogin : boolean = false
+        await PAGE.goto('https://account.proton.me/login', { waitUntil: 'networkidle0' })
+        await PAGE.waitForSelector('body > div.app-root > div.flex-no-min-children.flex-nowrap.flex-column.h100.sign-layout-bg.scroll-if-needed.relative > div.sign-layout-container.flex-item-fluid-auto.flex.flex-nowrap.flex-column.flex-justify-space-between > div > main > div.sign-layout-main-content > form > button')
+        await PAGE.type('#username', this._Username)
+        await PAGE.type('#password', this._Password)
+        await PAGE.click('body > div.app-root > div.flex-no-min-children.flex-nowrap.flex-column.h100.sign-layout-bg.scroll-if-needed.relative > div.sign-layout-container.flex-item-fluid-auto.flex.flex-nowrap.flex-column.flex-justify-space-between > div > main > div.sign-layout-main-content > form > button')
+        await PAGE.waitForSelector('body > div.app-root > div.flex.flex-row.flex-nowrap.h100 > div > div > div > div.sidebar.flex.flex-nowrap.flex-column.no-print.outline-none > div.flex-item-fluid.flex-nowrap.flex.flex-column.scroll-if-needed.pb1 > nav')
+            .then(async () => {
+                console.log("Logged in"); 
+            })
+            .catch(err => {  
+                invalidLogin = true;
+                throw new Error("Invalid login.")
+            })        
+        await PAGE.waitForNetworkIdle({ idleTime: 250 })
+        await PAGE.screenshot({ path: '5.png'})
+    }
 }
 
 export { ProtonMail };
